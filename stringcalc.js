e166daa5831e534abc4691238959c4b70c7824ad
Yug Vithani <vithaniyug01@gmail.com>
2025-07-05 16:48:38 +0530

    testcase: Including \n

--- stringcalc.js
+++ stringcalc.js
@@ -1,6 +1,6 @@
 const add = (ip) => {
     if(!ip) return 0;
-    return ip.split(',').reduce((sum, num) => sum + parseInt(num, 10), 0);
+    return ip.split("").filter(ch => ch!='\n' && ch!=',').reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
 module.exports = { add }
--- stringcalc.spec.js
+++ stringcalc.spec.js
@@ -2,12 +2,15 @@ const { add } = require('./stringcalc.js');
 
 describe("String Calculator Tests", () => {
     test("Empty String",() => {
-        expect(add("")).toBe(0);
+        expect(add("")).toBe(0)
     }),
     test("Single Number", () => {
-        expect(add("1")).toBe(1);
+        expect(add("1")).toBe(1)
     }),
     test("Multiple Numbers", () => {
-        expect(add("1,2")).toBe(3);
+        expect(add("1,2")).toBe(3)
+    }),
+    test("Including \\n", () => {
+        expect(add("1,2\n3")).toBe(6)
     })
 });
